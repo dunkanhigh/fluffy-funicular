589242ca8820c5bcd7b35b8ff964ffb71a4e1846
Эo_savinov <oleg.savinov.2002@bk.ru>
2025-04-16 17:36:47 +0300

    add Dag

---
+++ dags/covid_data.py
@@ -0,0 +1,41 @@
+from datetime import datetime, timedelta
+from io import BytesIO
+
+import boto3
+import kagglehub
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from kagglehub import KaggleDatasetAdapter
+
+default_args = {
+    'owner': 'o_savinov',
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+}
+
+def upload_dataset():
+    df = kagglehub.dataset_load(adapter=KaggleDatasetAdapter.PANDAS,
+                                handle="fireballbyedimyrnmom/us-counties-covid-19-dataset",
+                                path='us-counties.csv')
+    df = df.drop('fips', axis=1)
+    aws_access_key_id = 'ILjHRBpiGoHuBXwdp9zG'
+    aws_secret_access_key = '61iwwaFS6nIiDCULv8o4PcUUxqCAQInnvcJXRwC8'
+    minio_endpoint = 'http://localhost:9000'
+    bucket_name = 'covid'
+    s3 = boto3.client('s3', endpoint_url=minio_endpoint,
+                    aws_access_key_id=aws_access_key_id,
+                    aws_secret_access_key=aws_secret_access_key)
+    parquet_buffer = BytesIO()
+    df.to_parquet(parquet_buffer, engine='pyarrow')
+
+    s3.put_object(Bucket=bucket_name,
+                Key='test.parquet',
+                Body=parquet_buffer.getvalue())
+
+
+with DAG(dag_id='covid_data',
+         schedule="@daily",
+         start_date=datetime(2024, 1, 1),
+         default_args=default_args):
+    to_s3 = PythonOperator(task_id='get_and_upload_to_s3', python_callable=upload_dataset)
+    to_s3
